8d7ac8e0e7dca40c878f6068f86dba82bc5bc2db
samu <auces97@gmail.com>
2025-03-24 21:45:38 -0600

    finishing the base of the project, now cotainers are able to do what they are supposed to do, connect to DB show the data in API and use the API to show data to end user

--- microservices-api/app/routes.py
+++ microservices-api/app/routes.py
@@ -1,15 +1,20 @@
 from flask import Blueprint, jsonify
 from faker import Faker
-api_bp = Blueprint("api", __name__)
+from pymongo import MongoClient
 
 
 
-fake = Faker()
+
+#fake = Faker()
 anime_bp = Blueprint('anime', __name__)
+# Conectar a MongoDB
+client = MongoClient("mongodb://mongodb:27017/")  # Usar credenciales definidas en docker-compose
+db = client["dragonball"]  # Acceder a la base de datos dragonball
+collection = db["characters"]  # Acceder a la colección characters
 
 @anime_bp.route('/anime-characters', methods=['GET'])
 def get_anime_characters():
-    #data = [{"name": fake.first_name(), "power_level": "100", "anime": "Dragon Ball Z"} for _ in range(10)]
+    '''#data = [{"name": fake.first_name(), "power_level": "100", "anime": "Dragon Ball Z"} for _ in range(10)]
     #print(data)
     data = []
     for _ in range(10):  # Generar 10 personajes ficticios
@@ -19,4 +24,15 @@ def get_anime_characters():
             "anime": "Dragon Ball Z"
         })
         print (data)
+    return jsonify(data)'''
+    # Consultar la base de datos para obtener los personajes
+    characters = collection.find()
+    # Convertir los resultados a una lista de diccionarios para jsonify
+    data = []
+    for character in characters:
+        data.append({
+            "name": character["name"],
+            "power_level": character["power_level"],
+            "anime": "Dragon Ball Z"  # O cualquier otro dato si lo tienes en la DB
+        })
     return jsonify(data)
--- microservices-frontend/app/routes.py
+++ microservices-frontend/app/routes.py
@@ -8,3 +8,20 @@ def home():
     response = requests.get("http://api:5000/anime-characters")  # Conectamos a la API
     data = response.json()
     return render_template("index.html", characters=data)
+    #db = current_app.db
+    #characters_collection = db["characters"]
+    #characters = list(characters_collection.find({}, {"_id": 0}))  # Excluir _id
+    #return jsonify(characters)
+
+'''
+# Configurar la conexión con MongoDB
+app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost:27017/anime_db")
+mongo = PyMongo(app)
+
+# Endpoint para obtener personajes desde MongoDB
+@database_bp.route("/characters", methods=["GET"])
+def get_characters():
+    characters = list(mongo.db.characters.find({}, {"_id": 0}))  # Excluye _id para evitar problemas con JSON
+    return jsonify(characters)
+
+'''
